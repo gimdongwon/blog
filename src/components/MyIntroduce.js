6a36477a29a4af54488f0b2dd395a9ba41600878
kimdongwon <ehddnjs8989@gmail.com>
2018-07-18 18:53:30 +0900

    introduce edit

--- src/components/MyIntroduce.js
+++ src/components/MyIntroduce.js
@@ -27,15 +27,15 @@ export default class MyIntroduce extends React.Component {
   render() {
     return (
       <div>
-        <p>
-          Hihi! nice to meet you. My name is dongwon kim, I wanna be a front end
-          developer. <br />So, I am studying HTML, CSS, JavaScript, React.js,
-          Restful API, etc... <br />My dreams are a lot of things, one of thing
-          in strong wish is will be a great React-developer in korea. <br />Thank
-          you for reading this my writing. We will be able to friendly.
-        </p>
-
         <ul className="listItems">
+          <p>
+            Hihi! nice to meet you. My name is dongwon kim, I wanna be a front
+            end developer. <br />So, I am studying HTML, CSS, JavaScript,
+            React.js, Restful API, etc... <br />My dreams are a lot of things,
+            one of thing in strong wish is will be a great React-developer in
+            korea. <br />Thank you for reading this my writing. We will be able
+            to friendly.
+          </p>
           <li onClick={this.skilsClick} className="SkilsList">
             Skils
           </li>
